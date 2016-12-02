7666d68ec49a51559afa785f988862979be20ede
Xi Zhang <mxizhang1170@yahoo.com>
2016-12-02 15:44:39 -0500

    Add files via upload

--- ScrapyCountyFlip/zillow_function.py
+++ ScrapyCountyFlip/zillow_function.py
@@ -1,16 +1,14 @@
 import requests
 from lxml import html
-from pyzillow.pyzillow import ZillowWrapper, GetDeepSearchResults, ZillowError
 
 ZIL_KEY = 'X1-ZWz1fe5w83qcjv_70ucn'
 
 def findzillow(address, zipcode):
-	zillow_data = ZillowWrapper(ZIL_KEY)
 	if zipcode == '':
 		#print "No Zipcode"
 		address = address.replace(' ', '+')
 		url_search = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=' + ZIL_KEY +'&address=' + address + '&citystatezip=NJ'
-		print url_search
+		#print url_search
 
 		tree = html.fromstring(requests.get(url_search).content)
 		zpid = tree.xpath('//zpid/text()')
@@ -18,29 +16,30 @@ def findzillow(address, zipcode):
 		link = tree.xpath('//homedetails/text()')
 		zipcode = tree.xpath('//zipcode/text()')
 	else:
+		url_search = 'http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=' + ZIL_KEY +'&address=' + address + '&citystatezip=' + zipcode
+		#print url_search
+		'''
 		deep_search_response = zillow_data.get_deep_search_results(address, zipcode)
 		result = GetDeepSearchResults(deep_search_response)
 		zpid = [result.zillow_id]
 		#print zpid
 		url = 'http://www.zillow.com/webservice/GetZestimate.htm?zws-id=' + ZIL_KEY +'&zpid=' + zid
 		print url
-		page = requests.get(url)
-		tree = html.fromstring(page.content)
+		'''
+		tree = html.fromstring(requests.get(url_search).content)
+		zpid = tree.xpath('//zpid/text()')
 		prices = tree.xpath('//amount/text()')
 		link = tree.xpath('//homedetails/text()')
-		
+		zipcode = tree.xpath('//zipcode/text()')
 	try:
 		zillow = [zpid[0], prices[0], link[0], zipcode[0]]
 		return zillow
 
-	except ZillowError as err:
-		print ("No Zillow Found!")
-		return ['', '', '', '']
 	except IndexError as err:
 		print ("Index expecption!")
 		return ['', '', '', '']
 '''
-address = "141 MAIN STREET HAMPTON"
-z = findzillow(address, '')
+address = "159 GREGORY AVE WEST ORANGE NJ 07052"
+z = findzillow(address, '07052')
 print z
 '''
