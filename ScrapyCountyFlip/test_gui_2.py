e894f0e5b9c08e4fb5762f12a00b08033299e1a3
Xi Zhang <mxizhang1170@yahoo.com>
2017-01-09 19:01:11 -0500

    Add files via upload

---
+++ ScrapyCountyFlip/test_gui_2.py
@@ -0,0 +1,90 @@
+from Tkinter import *
+import scrapycounty
+import sys
+import subprocess
+
+morris = {'name': 'Morris', 'csv': 'morris_items.csv', 'date': 'Thr'}
+essex = {'name': 'Essex', 'csv': 'essex_items.csv', 'date': 'Tue'}
+bergen = {'name': 'Bergen', 'csv': 'bergen_items.csv', 'date': 'Fri'}
+hunterdon = {'name': 'Hunterdon', 'csv': 'hunterdon_items.csv', 'date': 'Wed'}
+union = {'name': 'Union', 'csv': 'union_items.csv', 'date': 'Wed'}
+mercer = {'name': 'Mercer', 'csv': 'mercer_items.csv', 'date': 'Wed'}
+middlesex = {'name': 'Middlesex', 'csv': 'middlesex_items.csv', 'date': 'Wed'}
+monmouth = {'name': 'Monmouth', 'csv': 'monmouth_items.csv', 'date': 'Mon'}
+passaic = {'name': 'Passaic', 'csv': 'passaic_items.csv', 'date': 'Tue'}
+
+COUNTY = [morris, essex, bergen, hunterdon, union, mercer, middlesex, monmouth, passaic]
+LENGTH = len(COUNTY)
+class simpleapp_tk(Tk):
+    def __init__(self, parent):
+        Tk.__init__(self, parent)
+        self.parent = parent
+        self.initialize()
+
+    def initialize(self):
+        self.grid()
+
+        global var
+        var = IntVar()
+        for index, item in enumerate(COUNTY):
+        	self.b = Radiobutton(self, text=item['name']+'(%s)' % item['date'], variable=var, value=index, command=self.sel)
+        	self.b.grid(column=0, row=index, columnspan=2, sticky='W')
+
+        self.hi_there = Button(self, text="Select", command=self.selectscrpy)
+        self.hi_there.grid(column=0, row=index+1, columnspan=2, sticky='EW')
+
+        self.text = Text(self, height=10, width=40)
+        self.text.grid(column=3,row=0, rowspan=index+1, columnspan=2, sticky='N')
+
+        sys.stdout = self
+        self.resizable(True,False)
+
+    def selectscrpy(self):
+    	self.button = Button(self, text="Run", fg="red", command=self.runscrapy)
+        self.button.grid(column=3, row=LENGTH, columnspan=2, sticky='EW')
+    	option = var.get()
+        self.entryVariable1 = StringVar()
+        self.entry1 = Entry(self, textvariable=self.entryVariable1)
+        self.entry1.grid(column=3, row=7, sticky='EW')
+        self.entryVariable1.set(u"Enter Old Tab Name Here:")
+        self.entryVariable2 = StringVar()
+        self.entry2 = Entry(self, textvariable=self.entryVariable2)
+        self.entry2.grid(column=3, row=8, sticky='EW')
+        self.entryVariable2.set(u"Download pdf? (Y/N)")
+        if option == 5 or option == 3:
+        	self.entry2.configure(state='normal')
+        else:
+        	self.entry2.configure(state='disabled')
+
+
+    def sel(self):
+		self.text.delete('1.0', "end")
+		option = var.get()
+		selection = "You selected the " + str(COUNTY[option]['name']) + " county.\n"
+		self.text.insert(INSERT, selection)
+
+		if option == 3:
+			self.text.insert(INSERT, "Hunterdon county is using .pdf\n")
+		elif option == 5:
+			self.text.insert(INSERT, "Mercer county is using .pdf\nPlease follow: \n")
+			self.text.insert(INSERT, "Step 1: Download sheriff_foreclosuresales_list.pdf from http://nj.gov/counties/mercer/pdfs/sheriff_foreclosuresales_list.pdf\n")
+			self.text.insert(INSERT, "Step 2: Open http://www.pdftoexcel.com/\n")
+			self.text.insert(INSERT, "Step 3: Upload sheriff_foreclosuresales_list.pdf and download .xlsx\n")
+			self.text.insert(INSERT, "Step 4: Save as sheriff_foreclosuresales_list.xlsx in ScrapyCounty_windows folder\n")
+
+    def write(self, txt):
+        self.text.insert(END, str(txt))
+
+    def runscrapy(self):
+        option = var.get()
+        sheetname = self.entryVariable1.get()
+
+        if option == 3:
+            scrapycounty.hunterdon(option, sheetname)
+        elif option == 5:
+            subprocess.call("python mercer_convert.py", shell=True)
+
+if __name__ == "__main__":
+    app = simpleapp_tk(None)
+    app.title('Scrapy County')
+    app.mainloop()
